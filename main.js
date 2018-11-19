8f7ecf1580b4a1e10414db739878d1bed79427f8
zak14 <zakariay14@gmail.com>
2018-11-19 09:55:57 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,44 @@
+// console.log("Hello World");
+
+// var zak;
+
+// zak = 14 + 89;
+
+// console.log(zak);
+// console.log(zak - 50);
+
+
+
+// var a = 1, b = 2, c = 3, d = 4, e = 5, f;
+// console.log(a, b, c, d, e);
+
+// f = e;
+// e = d;
+// d = c;
+// c = b;
+// b = a;
+// a = f;
+
+// console.log(a, b, c, d, e);
+// var a = 1, b = 2, c = 3, d = 4, e = 5, f;
+
+// f = a;
+// a = b;
+// b = c;
+// c = d;
+// d = e;
+// e = f;
+
+// console.log(a, b, c, d, e);
+
+var a = prompt("Insérez A");
+var b = prompt("Insérez B");
+console.log(a);
+console.log(b);
+
+if (a < b) {
+    console.log(a);
+}
+else {
+    console.log(b);
+}
